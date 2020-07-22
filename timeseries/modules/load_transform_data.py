26f4093ed89fc208ddacd41f8bc27283ecf00e21
Konstantin Schuckmann <kn.schuckmann@gmx.de>
2020-07-22 22:25:50 +0200

    created the overall sum for data

--- timeseries/modules/load_transform_data.py
+++ timeseries/modules/load_transform_data.py
@@ -8,9 +8,8 @@ Created on Wed Jul 22 11:53:34 2020
 
 import pandas as pd 
 import numpy as np 
+from pandas_profiling import ProfileReport
 
-import plotly
-import plotly.graph_objs as go
 
 '''
 - habe die daten in csv umgewandelt und in eine Exle eingefügt. überwiegend mit exel die gesamt transformation gemacht
@@ -33,48 +32,45 @@ def load_data(ORIG_DATA_PATH):
     return result_frame
 
 
-def check_for_nan(data):
+
+def create_overall_sum_df(data):
     
-    result={}
-    for nr, df in enumerate(data):
-        if df.isnull().values.any():
-            result.update({nr:True})
-    
-    if not result:
-        result = False
+    for nr, dat in enumerate(data):
+        data[nr] = dat.set_index('Verkaufsdatum')
+        
+    result = data[0]
+    result['Tages Menge in ST'] = np.zeros(len(result))
+    result['Tages Wert in EUR'] = np.zeros(len(result))
+
+    for _, dat in enumerate(data[1:]):
+        if dat.columns.str.match('Einzel').any() == True:
+            result['Einzel Menge in ST'] = result['Einzel Menge in ST'].add(dat['Einzel Menge in ST'], fill_value = 0)  
+            result['Einzel Wert in EUR'] = result['Einzel Wert in EUR'].add(dat['Einzel Wert in EUR'], fill_value = 0)
+            if dat.columns.str.match('4Fahrt').any() == True:
+                result['4Fahrt Menge in ST'] = result['4Fahrt Menge in ST'].add(dat['4Fahrt Menge in ST'], fill_value = 0)  
+                result['4Fahrt Wert in EUR'] = result['4Fahrt Wert in EUR'].add(dat['4Fahrt Wert in EUR'], fill_value = 0)  
+        else:
+            result['Tages Menge in ST'] = result['Tages Menge in ST'].add(dat['Tages Menge in ST'], fill_value = 0)  
+            result['Tages Wert in EUR'] = result['Tages Wert in EUR'].add(dat['Tages Wert in EUR'], fill_value = 0)  
+            
+        result['Gesamt Menge in ST'] = result['Gesamt Menge in ST'].add(dat['Gesamt Menge in ST'], fill_value = 0)  
+        result['Gesamt Wert in EUR'] = result['Gesamt Wert in EUR'].add(dat['Gesamt Wert in EUR'], fill_value = 0)  
     
     return result
 
-
-import plotly.express as px
-df = einzel_aut
-#fig = px.scatter_3d(df, x='Verkaufsdatum', y='Einzel Wert in EUR', z='Einzel Menge in ST')
-fig = px.histogram(df, x="Einzel Wert in EUR") # histogram
-fig = px.histogram(df, x="Einzel Menge in ST")
-plotly.offline.plot(fig, filename = 'test')
-
-    
 def main():
     
     data = load_data(ORIG_DATA_PATH)
     
+    overall_data = create_overall_sum_df(data)
     
-    einzel_aut = data[0]
-    einzel_aut
-    einzel_eigVkSt = data[1] 
-    einzel_privat = data[2] 
-    einzel_bus = data[3] 
-    einzel_app = data[4] 
-    tages_aut = data[5] 
-    tages_eigVkSt = data[6] 
-    tages_privat = data[7] 
-    tages_bus = data[8] 
-    tages_app = data[9] 
+    einzel_aut , einzel_eigVkSt, einzel_privat, einzel_bus, einzel_app, tages_aut, tages_eigVkSt, tages_privat, tages_bus, tages_app = data    
+   
     
     
+    profile = ProfileReport(einzel_aut, title="Pandas Profiling Report")
     
-    
-    
+    profile.to_file("your_report.html")
     
 if __name__  == '__main__' : 
     main()
--- timeseries/modules/test.py
+++ timeseries/modules/test.py
@@ -6,6 +6,17 @@ Created on Wed Jul 22 17:49:55 2020
 @author: Kostja
 """
 
+import plotly
+import plotly.graph_objs as go
+
+import plotly.express as px
+df = einzel_aut
+#fig = px.scatter_3d(df, x='Verkaufsdatum', y='Einzel Wert in EUR', z='Einzel Menge in ST')
+fig = px.histogram(df, x="Einzel Wert in EUR") # histogram
+fig = px.histogram(df, x="Einzel Menge in ST")
+plotly.offline.plot(fig, filename = 'test')
+
+    
 trace0 = go.Scatter(
     x = einzel_aut['Verkaufsdatum'],
     y = einzel_aut['Einzel Menge in ST'],
@@ -65,3 +76,20 @@ trace0 = go.Surface(z=einzel_aut.values)
 plt = [trace0]
 
 plotly.offline.plot(plt, filename = 'test')
+
+
+
+def check_for_nan(data):
+    
+    result={}
+    for nr, df in enumerate(data):
+        if df.isnull().values.any():
+            result.update({nr:True})
+    
+    if not result:
+        result = False
+    
+    return result
+
+if check_for_nan(data):
+    print('The DataFrame has Nan´s in it, with following form {nr_datafarme, True for Nans}: ' + str(check_for_nan(data)))
