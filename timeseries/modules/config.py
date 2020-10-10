d7b2faaa1ae1f2e71ae88ab83707c756be5c2a7c
Konstantin Schuckmann <kn.schuckmann@gmx.de>
2020-10-10 23:43:16 +0200

    created config file to store all paths

---
+++ timeseries/modules/config.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 10 15:03:51 2020
+
+@author: Kostja
+"""
+############################################################################################# 
+############################################ DUMMY ##########################################
+#############################################################################################
+dummy_temperature_path = '../resource_data/dummy/Bias_correction_ucl.csv' 
+dummy_flight_path = 'https://raw.githubusercontent.com/jbrownlee/Datasets/master/airline-passengers.csv'
+dummy_save_path_section_2 = '../../Latex/bhtThesis/Masters/2_fundamentals/pictures'
+dummy_save_path_section_3 = '../../Latex/bhtThesis/Masters/3_used_Models/pictures/'
+
+
+############################################################################################# 
+################################# LOAD AND TRANSFORM DATA ###################################
+#############################################################################################
+ORIG_DATA_PATH = './timeseries/resource_data/original/Transformed Daten Beuth 2019.03.15.xlsx'
+MONTH_DATA_PATH = './timeseries/resource_data/original/Monatsdaten ab 2012_fuer FC-V_in Stueck-1_CLEANED.xlsx'
