bae03cd88b4fcf3fb3c21cd069a39d2bb737d6ee
Konstantin Schuckmann <kn.schuckmann@gmx.de>
2020-11-08 20:53:00 +0100

    Added paths to config file for sophisticated prediction and monthly aggregation

--- timeseries/modules/config.py
+++ timeseries/modules/config.py
@@ -19,8 +19,9 @@ dummy_save_path_section_3 = '../../../../Latex/bhtThesis/Masters/3_used_Models/p
 #############################################################################################
 ORIG_DATA_PATH = '../resource_data/original/Transformed Daten Beuth 2019.03.15.xlsx'
 MONTH_DATA_PATH = '../resource_data/original/Monatsdaten ab 2012_fuer FC-V_in Stueck-1_CLEANED.xlsx'
-DATA = 'combined_df'
+DATA = 'combined_df'# 'df_0_einzel_aut'
 SAVE_PLOTS_PATH = '../../../../Latex/bhtThesis/Masters/5_evaluation/pictures/more_steps/Gesamt/' + DATA + '/'
 SAVE_MODELS_PATH = '../resource_data/models/more_steps/Gesamt/' + DATA + '/'
 SAVE_RESULTS_PATH = '../resource_data/models/results/'
 ORIGINAL_PATH = '../resource_data/original/'
+MODELS_PATH = '../resource_data/models/'
